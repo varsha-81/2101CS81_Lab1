ac1978997932a6382434e12ac5b2e3bb48fa3fb1
Varsha <95608041+varsha-81@users.noreply.github.com>
2022-07-27 20:45:06 +0530

    Code till taking input

---
+++ calculator_program.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+char operator;
+float n1, n2, result=0;
+printf ("Choose any operators from +, -, *, / :\n");
+scanf ("%c", &operator);
+printf("Enter values of n1 and n2\n");
+scanf("%f %f", &n1, &n2);
+//Enter the values of n1 and n2 with a space in between
