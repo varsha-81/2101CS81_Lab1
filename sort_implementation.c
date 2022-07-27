fbef45b3707975ad6275f786c5cd5561cdd122c9
Varsha <95608041+varsha-81@users.noreply.github.com>
2022-07-27 21:27:47 +0530

    Bubble sort function

--- sort_implementation.c
+++ sort_implementation.c
@@ -56,3 +56,18 @@ void display(int arr[],int n)
   printf("%d", arr[i]);
 }
 }
+void bubble_sort(int arr[], int n)
+{
+  int i,j,temp;
+  for(i=0;i<n;i++)
+  {
+    for(j=0;j<n-i-1;j++)
+    {
+      if(arr[j]>arr[j+1])
+      {
+        temp= arr[j];
+        arr[j]=arr[j+1];
+        arr[j+1]=temp;
+      }}}
+  printf("Elements after bubble sort:");
+  display(arr,n);}
