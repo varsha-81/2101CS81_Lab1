55dc6a18211b46644ba66a57df3a84408bf50858
Varsha <95608041+varsha-81@users.noreply.github.com>
2022-07-27 21:21:27 +0530

    switch for sorts

--- sort_implementation.c
+++ sort_implementation.c
@@ -17,4 +17,36 @@ int main() {
    scanf ("%d", &arr[i]);
   } printf("Select any one for sorting:\n");
   
-           
+   
+  while (1)
+  { printf ("\n1.Bubble Sort\n2.Selection Sort\n3.Insertion Sort\n4.Quick Sort\n5.Merge Sort\n6.Display Array\n7.Conclude the program.\n");
+   printf("\nEnter any choice:");
+   scanf("%d", &choice);
+   
+   switch(choice)
+   {
+     case 1:
+       bubble_sort(arr,n);
+       break;
+     case 2:
+       selection_sort(arr,n);
+       break;
+     case 3:
+       insertion_sort(arr,n);
+       break;
+     case 4:
+       quick_sort(arr,n);
+       break;
+     case 5:
+       merge_sort(arr,n);
+       break;
+     case 6:
+       display(arr,n);
+       break;
+     case 7:
+       return 0;
+     default:
+       printf("Select options only from 1 to 7\n");}
+  }
+  return 0;}
+
