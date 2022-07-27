5e5826c32812f2d2f4177e306146b985ebc8ac59
Varsha <95608041+varsha-81@users.noreply.github.com>
2022-07-27 21:12:10 +0530

    specifying sorts

--- sort_implementation.c
+++ sort_implementation.c
@@ -6,3 +6,15 @@ void insertion_sort (int a[], int n);
 void quick_sort (int a[], int n);
 void merge_sort (int a[], int n);
 
+int main() {
+  int n, choice, i;
+  char ch[20];
+  printf("Enter no.of elements for sorting:");
+  scanf("%d",&n);
+  int arr[n];
+  for (i=0; i<n; i++)
+  { printf("Enter %d element: ", i+1);
+   scanf ("%d", &arr[i]);
+  } printf("Select any one for sorting:\n");
+  
+           
