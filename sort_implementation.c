a2d1a182ab4381049be58fcb50c7b07d94f56cfd
Varsha <95608041+varsha-81@users.noreply.github.com>
2022-07-27 21:02:50 +0530

    Main Function

---
+++ sort_implementation.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void display (int a[], int n);
+void bubble_sort (int a[], int n);
+void selection_sort (int a[], int n);
+void insertion_sort (int a[], int n);
+void quick_sort (int a[], int n);
+void merge_sort (int a[], int n);
+
